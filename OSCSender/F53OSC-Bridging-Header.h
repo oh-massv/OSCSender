273a520714fa69542510cf7c3106da3ea9d651d7
massv <trk0364@gmail.com>
2019-06-10 10:18:30 +0900

    Create OSCSender Project and Add "F53OSC"

---
+++ OSCSender/F53OSC-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  F53OSC-Bridging-Header.h
+//  OSCSender
+//
+//  Created by natari on 2019/06/10.
+//  Copyright © 2019 massv. All rights reserved.
+//
+
+#ifndef F53OSC_Bridging_Header_h
+#define F53OSC_Bridging_Header_h
+
+#import "F53OSC.h"
+
+#endif /* F53OSC_Bridging_Header_h */
--- OSCSender/ViewController.swift
+++ OSCSender/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let oscClient = F53OSCClient.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        oscClient.host = "172.24.21.99"
+        oscClient.port = 3333
     }
-
-
+    
+    @IBAction func sendOSC(){
+        //123というメッセージをOSCで送信
+        self.sendMessage(client: oscClient, addressPattern: "/value", arguments: [123 as AnyObject])
+        //複数の値を送る場合はarguments:[123,231,312]
+    }
+    
+    func sendMessage(client: F53OSCClient, addressPattern: String, arguments: [AnyObject]) {
+        let message: F53OSCPacket = F53OSCMessage(addressPattern: addressPattern, arguments: arguments)
+        client.send(message)
+        print("Sent '\(message)' to \(client.host):\(client.port)")
+    }
 }
-
